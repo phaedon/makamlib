d5bacce9bfd6dc78a5dd9a97027a93601be4e540
Phaedon Sinis <phaedonsinis@gmail.com>
2013-09-26 21:43:56 -0700

    creating a few more json files

--- ruby/pitches.rb
+++ ruby/pitches.rb
@@ -1,14 +1,40 @@
 require 'json'
 
-file = open("../json/pitches.json")
-jsonPitches = file.read
-parsed = JSON.parse(jsonPitches)
-#file.close
-
 seeds = File.new("seeds.rb", "w")
+
+def parseJSON(filename)
+  fullfilename = "../json/" + filename + ".json"
+  file = open(fullfilename)
+  jsontext = file.read
+  parsed = JSON.parse(jsontext)
+  file.close
+  return parsed
+end
+
+parsed = parseJSON("pitches")
+seeds.puts "Pitch.delete_all"
 parsed.each do |key, value|
   seeds.puts "Pitch.create!(name: '" + key + "')"
 end
-seeds.close  
 
+parsed = parseJSON("makams")
+seeds.puts "Makam.delete_all"
+parsed.each do |key, value|
+#  seeds.puts "Makam.create!(name: '" + value["name"] + "', karar: '" + value["karar"] + "')"
+  seeds.puts "Makam.create!(name: '" + value["name"] + "')"
+end
+
+parsed = parseJSON("instruments")
+seeds.puts "Instrument.delete_all"
+parsed.each do |key|
+  seeds.puts "Instrument.create!(name: '" + key + "')"
+end
+
+parsed = parseJSON("performers")
+seeds.puts "Performer.delete_all"
+parsed.each do |key|
+  seeds.puts "Performer.create!(name: '" + key + "')"
+end
+
+seeds.close
 
